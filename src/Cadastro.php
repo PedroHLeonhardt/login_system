a3b02ee663e983717c3844a55ec14268a2fad360
leonhardt <pedrohvfleonhardt@gmail.com>
2025-10-01 14:47:16 -0300

    Adicionado classe Usuario com construtor e métodos de acesso (getters/setters)

---
+++ src/Cadastro.php
@@ -0,0 +1,43 @@
+<?php
+
+class Usuario 
+{
+    private string $name;
+    private string $email;
+    private string $password;
+
+    public function __construct($name, $email, $password)
+    {
+        $this->setName($name);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
